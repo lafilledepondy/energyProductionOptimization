6132f502600f0a2ecc87e5f413e64988c695dd7e
Delphinnoel <delphin.noel0@gmail.com>
2026-03-15 04:46:01 +0100

    Classe et Fonction de reading

---
+++ src/data.py
@@ -0,0 +1,234 @@
+import sys
+import re
+
+    
+class power1 :
+    def __init__(self, index, S):
+        self._name = index
+        self._scenario = S
+        self._pmin = []
+        self._pmax = []
+        self._cost = []
+    
+class campaign :
+    def __init__(self, index):
+        self._name = index
+        self._earlieststop = 0
+        self._lateststop = 0
+        self._durationoutage = 0 
+        self._maxrefuel = 0
+        self._minrefuel = 0 
+        self._maxstock = 0 
+        self._refuelingcost = 0
+
+
+    def index(self):
+        return self._name
+
+    def earlieststop(self):
+        return self._earlieststop
+    
+    def lateststop(self):
+        return self._lateststop
+    
+    def durationoutage(self):
+        return self._durationoutage
+    
+class scenario :
+    def __init__(self, index):
+        self._name = index
+        self._demands= []
+        self._Power1 = []
+
+    def addPower1(self, power1):
+        self._Power1.append(power1)
+
+
+    def index(self):
+        return self._name
+
+    def nbdemands(self):
+        return self._demands
+
+
+class power2 :
+    def __init__(self, index):
+        self._name = index
+        self._initialstock = 0
+        self._minstock = 0
+        self._Campaigns = []
+        self._pmax = []
+    
+    def addCampaign(self, campaign):
+        self._Campaigns.append(campaign)
+
+class alldata :
+    def __init__(self, name):
+        self._name = name
+        self._timestep = 0
+        self._weeks = 0
+        self._nbcampaigns = 0
+        self._nbscenario = 0
+        self._nbpower1 = 0
+        self._nbpower2 = 0
+        self._timestepduration = [] 
+        self._Scenario = []
+        self._Power2 = []
+    
+    def addScenario(self, scenario):
+        self._Scenario.append(scenario)
+
+    def addPower2(self, power2):
+        self._Power2.append(power2)
+
+
+        
+
+
+def Readingfile(dataFilePath):
+    try:
+        
+        actuel = "None"
+        compte = 0
+        type1 = -1
+        type2 = -1
+        
+        with open(dataFilePath, 'r') as dataFile:
+            import os
+            dataFileName = os.path.splitext(os.path.basename(dataFilePath))[0]
+            
+            data = alldata(dataFileName)
+            
+            for ligne in dataFile :
+                ligne = ligne.strip()
+                if not ligne:
+                    continue
+
+                if ligne.startswith("begin main") :
+                    actuel = "main"
+                if actuel == "main":
+                    if ligne.startswith("timestep") :
+                        data._timestep = int(ligne.split()[1])
+                    if ligne.startswith("weeks") :
+                        data._weeks= int(ligne.split()[1])
+                    if ligne.startswith("campaigns") :
+                        data._nbcampaigns = int(ligne.split()[1])
+                    if ligne.startswith("scenario") :
+                        data._nbscenario = int(ligne.split()[1])
+                    if ligne.startswith("powerplant1") :
+                        data._nbpower1 = int(ligne.split()[1])
+                    if ligne.startswith("powerplant2") :
+                        data._nbpower2 = int(ligne.split()[1])
+                    if ligne.startswith("durations"):
+                        data._timestepduration = [int(n) for n in ligne.split()[1:]]
+                    if ligne.startswith("demand"):
+                        index = "scenario_" + str(compte)
+                        s = scenario(index)
+                        s._demands = [float(n) for n in ligne.split()[1:]]
+                        compte += 1
+                        data.addScenario(s)
+                        
+                        
+
+                if ligne.startswith("type 1") :
+                    actuel = "type 1"
+                    compte = 0
+                    type1 += 1
+                    index = "powerplant_1_" + str(type1)
+                    
+
+                if actuel == "type 1":
+                    if ligne.startswith("pmin"):
+                        p1 = power1(index, compte)
+                        p1._pmin = [float(n) for n in ligne.split()[1:]]
+                    if ligne.startswith("pmax"):
+                        p1._pmax = [float(n) for n in ligne.split()[1:]]
+                    if ligne.startswith("cost"):
+                        p1._cost = [float(n) for n in ligne.split()[1:]]
+                        
+                        data._Scenario[compte].addPower1(p1)
+                        compte += 1
+                        
+                    
+
+                if ligne.startswith("type 2") :
+                    actuel = "type 2"
+                    compte = 0
+                    type2 += 1
+                    index = "powerplant_2_" + str(type2)
+                    p2 = power2(index)
+                if actuel == "type 2":
+                    
+                    if ligne.startswith("stock") :
+                        p2._initialstock = int(ligne.split()[1])
+                    if ligne.startswith("stock") :
+                        p2._initialstock = int(ligne.split()[1])
+                    if ligne.startswith("durations") :
+                        duree = ligne.split()[1:]
+                        n = len(duree)
+                        
+                        for i in range(n):
+                            index2 = "campaign_" + str(i)
+                            camp = campaign(index2)
+                            camp._durationoutage = int(duree[i] )
+                            p2.addCampaign(camp)
+                               
+                        
+                        
+                    if ligne.startswith("max_refuel") :
+                        duree = ligne.split()[1:]
+                        n = len(duree) 
+                        
+                        for i in range(n):  
+                            p2._Campaigns[i]._maxrefuel = duree[i] 
+                        
+                        
+                    if ligne.startswith("min_refuel") :
+                        duree = ligne.split()[1:]
+                        n = len(duree) 
+                        for i in range(n):
+                            p2._Campaigns[i]._minrefuel = duree[i]
+                    if ligne.startswith("current_campaign_stock_threshold"):
+                        p2._minstock = int(ligne.split()[1])
+                    if ligne.startswith("pmax"):
+                        p2._pmax = [float(n) for n in ligne.split()[1:]]
+                    if ligne.startswith("max_stock_after_refueling") :
+                        duree = ligne.split()[1:]
+                        n = len(duree) 
+                        for i in range(n):
+                            p2._Campaigns[i]._maxstock = duree[i]
+                    if ligne.startswith("refueling_cost") :
+                        duree = ligne.split()[1:]
+                        n = len(duree) 
+                        for i in range(n):
+                            p2._Campaigns[i]._refuelingcost = duree[i]
+                        data.addPower2(p2)
+                        
+
+                if ligne.startswith("type 13") :
+                    actuel = "type 13"
+                    p = 0
+                    c = 0
+                    if ligne.startswith("powerplant") :
+                        p = int(ligne.split()[1])
+                    if ligne.startswith("campaign") :
+                        c = int(ligne.split()[1])
+                    if ligne.startswith("earliest_stop_time") :
+                        e = int(ligne.split()[1])
+                        data._Power2[p]._Campaigns[c]._earlieststop = e
+                    if ligne.startswith("earliest_stop_time") :
+                        e = int(ligne.split()[1])
+                        data._Power2[p]._Campaigns[c]._lateststop = e 
+            
+            return data
+                    
+
+    except IOError as e:
+        print("I/O error({0}): {1}".format(e.errno, e.strerror), file=sys.stderr)
+        sys.exit(1)
+    except ValueError:
+        print("Could not convert data to an integer.", file=sys.stderr)
+        sys.exit(1)
+    except:
+        print("Unexpected error:", sys.exc_info()[0], file=sys.stderr)
+        sys.exit(1)
