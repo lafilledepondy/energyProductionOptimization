d6e4e4d0de6e0497aa4ff7b55036c60d5406ac73
gaia <gayathiri.ravendirane@gmail.com>
2026-05-18 21:57:17 +0200

    renammed the heuristics

--- src/demo.py
+++ src/demo.py
@@ -52,18 +52,18 @@ def heuristic_2_demo(file_name: str, scheme:int, optimal_value: float = None):
         from .data import Readingfile
         from .checker import Checker
         from .solution import print_solution
-        from .heristiques import MaintenanceHeuristicV2_basic
+        from .heristiques import MaintenanceHeuristic_basic
     except ImportError:
         from data import Readingfile
         from checker import Checker
         from solution import print_solution
-        from heristiques import MaintenanceHeuristicV2_basic
+        from heristiques import MaintenanceHeuristic_basic
 
     print("Running heuristic 2 on instance:", file_name, "with scenario", scheme)
     data_file = Path(__file__).resolve().parents[1] / "data" / "Base_A" / file_name
     data = Readingfile(str(data_file))
 
-    heuristic = MaintenanceHeuristicV2_basic()
+    heuristic = MaintenanceHeuristic_basic()
     sol = heuristic.solve(data, scheme)
 
     if sol is None:
@@ -82,18 +82,18 @@ def heuristic_2_2_demo(file_name: str, scheme:int, optimal_value: float = None):
         from .data import Readingfile
         from .checker import Checker
         from .solution import print_solution
-        from .heristiques import MaintenanceHeuristicV2MultiCampaign
+        from .heristiques import MaintenanceHeuristic_MultiCampaign
     except ImportError:
         from data import Readingfile
         from checker import Checker
         from solution import print_solution
-        from heristiques import MaintenanceHeuristicV2MultiCampaign
+        from heristiques import MaintenanceHeuristic_MultiCampaign
 
     print("Running heuristic 2_2 on instance:", file_name, "with scenario", scheme)
     data_file = Path(__file__).resolve().parents[1] / "data" / "Base_A" / file_name
     data = Readingfile(str(data_file))
 
-    heuristic = MaintenanceHeuristicV2MultiCampaign()
+    heuristic = MaintenanceHeuristic_MultiCampaign()
     sol = heuristic.solve(data, scheme)
 
     if sol is None:
@@ -113,18 +113,18 @@ def heuristic_2_2_1demo(file_name: str, scheme:int, optimal_value: float = None)
         from .data import Readingfile
         from .checker import Checker
         from .solution import print_solution
-        from .heristiques import MaintenanceHeuristicV2MultiCampaign_2
+        from .heristiques import MaintenanceHeuristic_OptXandY
     except ImportError:
         from data import Readingfile
         from checker import Checker
         from solution import print_solution
-        from heristiques import MaintenanceHeuristicV2MultiCampaign_2
+        from heristiques import MaintenanceHeuristic_OptXandY
 
     print("Running heuristic 2_2_1 on instance:", file_name, "with scenario", scheme)
     data_file = Path(__file__).resolve().parents[1] / "data" / "Base_A" / file_name
     data = Readingfile(str(data_file))
 
-    heuristic = MaintenanceHeuristicV2MultiCampaign_2()
+    heuristic = MaintenanceHeuristic_OptXandY()
     sol = heuristic.solve(data, scheme)
 
     if sol is None:
@@ -144,18 +144,18 @@ def heuristic_2_RF_demo(file_name: str, scheme:int, optimal_value: float = None)
         from .data import Readingfile
         from .checker import Checker
         from .solution import print_solution
-        from .heristiques import MaintenanceHeuristicV2_RF
+        from .heristiques import MaintenanceHeuristic_RF
     except ImportError:
         from data import Readingfile
         from checker import Checker
         from solution import print_solution
-        from heristiques import MaintenanceHeuristicV2_RF
+        from heristiques import MaintenanceHeuristic_RF
 
     print("Running heuristic 2_RF on instance:", file_name, "with scenario", scheme)
     data_file = Path(__file__).resolve().parents[1] / "data" / "Base_A" / file_name
     data = Readingfile(str(data_file))
 
-    heuristic = MaintenanceHeuristicV2_RF()
+    heuristic = MaintenanceHeuristic_RF()
     sol = heuristic.solve(data, scheme)
 
     if sol is None:
@@ -174,18 +174,18 @@ def heuristic_3_dichotomie_demo(file_name: str, scheme:int, optimal_value: float
         from .data import Readingfile
         from .checker import Checker
         from .solution import print_solution
-        from .heristiques import MaintenanceHeuristicV3_dichotomie
+        from .heristiques import MaintenanceHeuristic_relaxLagrange
     except ImportError:
         from data import Readingfile
         from checker import Checker
         from solution import print_solution
-        from heristiques import MaintenanceHeuristicV3_dichotomie
+        from heristiques import MaintenanceHeuristic_relaxLagrange
 
     print("Running heuristic 3_dichotomie on instance:", file_name, "with scenario", scheme)
     data_file = Path(__file__).resolve().parents[1] / "data" / "Base_A" / file_name
     data = Readingfile(str(data_file))
     
-    heuristic = MaintenanceHeuristicV3_dichotomie(data, scheme)
+    heuristic = MaintenanceHeuristic_relaxLagrange(data, scheme)
     sol , soldual = heuristic.solve(data, scheme)
 
     if sol is None:
--- src/heristiques.py
+++ src/heristiques.py
@@ -101,287 +101,10 @@ class AbstractMaintenanceHeuristic:
     def solve(self, data: Readingfile, scenario: int) -> Solution:
         raise NotImplementedError
 
-# # -----------------------------
-# #  Heuristic 1
-# # -----------------------------
-# class MaintenanceHeuristicV1(AbstractMaintenanceHeuristic): 
-#     def computePriorityScores(self, data: Readingfile) -> list[tuple[int, float]]:
-#         w_i_tab = []
-#         I2 = range(data.nbpower2())
-#         Xi1 = [data.accessPower2(i).initialstock() for i in I2]
-#         Sth_min = [data.accessPower2(i).minstock() for i in I2]
-#         campaign_ids_by_unit = [range(len(data.accessPower2(i).Campaigns())) for i in I2]
-#         Smax_ik = [
-#             [
-#                 data.accessCampaign(i, k).maxstock()
-#                 for k in campaign_ids_by_unit[i]
-#             ]
-#             for i in I2
-#         ]
-#         T = range(data.timestep()) 
-#         Pmax_2 = [
-#             [data.accessPower2(i).pmax()[t] for t in T]
-#             for i in I2
-#         ]  # Type 2 units: Pmax_2[i][t]
-#         tot_Pmax_ik = [
-#             sum(Pmax_2[i][t] for t in T) for i in I2
-#         ]
-#         horizon_last_t = data.timestep() - 1
-#         K_i = []
-#         for i in I2:
-#             campaigns_i = []
-        
-#             for k in campaign_ids_by_unit[i]:
-#                 start = max(0, data.accessCampaign(i, k).earlieststop())
-#                 end   = min(horizon_last_t, data.accessCampaign(i, k).lateststop())
-            
-#                 k_range = list(range(start, end + 1))
-            
-#                 if k_range != [0]:   
-#                     campaigns_i.append(k_range)
-#             K_i.append(campaigns_i)
-
-#         K_i_simple = {}
-#         for i in I2:
-#             K_i_simple[i] = [t for campagne in K_i[i] for t in campagne]
-            
-
-#         for i in I2:
-#             if all(v == Smax_ik[i][0] for v in Smax_ik[i]):
-#                 smax_i = Smax_ik[i][0]
-#             else:
-#                 print(f"Warning: Smax_ik values are not the same for all campaigns of unit {i}.")
-                            
-#             Wi = (Xi1[i] - Sth_min[i]) / (smax_i - Sth_min[i]) # stock (Xi1 - Sth_min_i) / (Smax_ik - Sth_min_i)
-#             Wi += sum(Pmax_2[i][t] for t in T) / tot_Pmax_ik[i] # puissance
-#             Wi += 1 - (len(K_i_simple[i])/(data.timestep())) # fenetres de maintenance
-
-#             w_i_tab.append((i, Wi))
-
-#         return w_i_tab
-
-#     def sortPlantsByPriority(self, priorityScores: list[tuple[int, float]]) -> list[tuple[int, float]]:
-#         return sorted(priorityScores, key=lambda x: x[1], reverse=True) # tri decroissant
-
-#     def findFeasibleStartTime(
-#         self,
-#         data: Readingfile,
-#         i: int,
-#         demand: list[float],
-#         remaining_capacity: list[float],
-#     ) -> tuple[int, int] | None:
-#         plant = data.accessPower2(i)
-#         T = data.timestep()
-
-#         # !(initial stock >= min stock)
-#         if plant.initialstock() < plant.minstock():
-#             return None
-
-#         for k_index, campaign in enumerate(plant.Campaigns()):
-#             duration = campaign.durationoutage()
-#             start_min = max(0, campaign.earlieststop())
-#             start_max = min(campaign.lateststop(), T - duration)
-
-#             if start_min > start_max:
-#                 continue
-
-#             for t_start in range(start_min, start_max + 1):
-#                 feasible = True
-
-#                 for t in range(t_start, t_start + duration):
-#                     # cdt 1 (demande) et 2 (stock)
-#                     if remaining_capacity[t] - plant.pmax()[t] < demand[t]:
-#                         feasible = False
-#                         break
-
-#                 if feasible:
-#                     return k_index, t_start
-
-#         return None
-
-#     def scheduleMaintenance(self, data: Readingfile) -> tuple[list[list[int]], list[list[tuple[int, int]]]] | None:
-#         T = data.timestep()
-#         I2 = data.nbpower2()
-#         y_it = [[0 for _ in range(T)] for _ in range(I2)]
-#         x_itk = [[] for _ in range(I2)]
-
-#         # etape 1
-#         scores = self.computePriorityScores(data)
-#         ordered_plants = self.sortPlantsByPriority(scores)
-
-#         # etape 2
-#         scenario = data.accessScenario(0)
-#         demand = scenario.demands()[:]
-
-#         # generation capacity per timestep before scheduling outages
-#         remaining_capacity = [0.0 for _ in range(T)]
-#         for t in range(T):
-#             total_pmax_type1 = sum(
-#                 data.accessPower1(0, j).pmax()[t] for j in range(data.nbpower1())
-#             )
-#             total_pmax_type2 = sum(
-#                 data.accessPower2(j).pmax()[t] for j in range(data.nbpower2())
-#             )
-#             remaining_capacity[t] = total_pmax_type1 + total_pmax_type2
-
-#         # etape 3
-#         for (i, _) in ordered_plants:
-#             result = self.findFeasibleStartTime(data, i, demand, remaining_capacity)
-
-#             if result is None:
-#                 return None
-
-#             k_index, t_start = result
-#             plant = data.accessPower2(i)
-#             duration = plant.Campaigns()[k_index].durationoutage()
-#             x_itk[i].append((k_index, t_start)) # stock le choix
-
-#             for t in range(t_start, t_start + duration):
-#                 y_it[i][t] = 1
-#                 remaining_capacity[t] -= plant.pmax()[t]
-
-#         return y_it, x_itk
-
-#     def computeProductionPlan(
-#         self,
-#         data: Readingfile,
-#         y_it: list[list[int]],
-#         x_itk: list[list[tuple[int, int]]],
-#     ) -> tuple[
-#         dict[tuple[int, int], float],
-#         dict[tuple[int, int], float],
-#         dict[tuple[int, int], float],
-#         dict[tuple[int, int], float],
-#     ]:
-#         T = data.timestep()
-#         # p1_sol = {
-#         #     (i, t): float(data.accessPower1(0, i).pmax()[t])
-#         #     for i in range(data.nbpower1())
-#         #     for t in range(T)
-#         # }
-#         p1_sol: dict[tuple[int, int], float] = {}
-#         p2_sol: dict[tuple[int, int], float] = {}
-#         r_sol: dict[tuple[int, int], float] = {}
-#         s_sol: dict[tuple[int, int], float] = {}
-
-#         scheduled_campaign_start = {
-#             i: {t_start: k_index for k_index, t_start in x_itk[i]}
-#             for i in range(data.nbpower2())
-#         }
-#         productiontot = [0 for t in range(T)]
-#         for i in range(data.nbpower2()):
-#             plant = data.accessPower2(i)
-#             stock = float(plant.initialstock())
-
-#             for t in range(T):
-#                 if t in scheduled_campaign_start[i]:
-#                     campaign = plant.Campaigns()[scheduled_campaign_start[i][t]]
-#                     target_stock = float(campaign.maxstock())
-#                     refuel = max(0.0, campaign.maxrefuel()) #target_stock - stock  Modif pour produire que ce qu'on peut
-#                     if refuel > 0:
-#                         r_sol[(i, t)] = refuel
-#                     stock += refuel
-
-#                 production = 0.0 if y_it[i][t] == 1 else min(float(plant.pmax()[t]), data.accessScenario(0).demands()[t]- productiontot[t]) # Modif pour produire que si on en a besoin sinon on produit max possible
-#                 if stock - plant.minstock()*0.1 - production* float(data.timestepduration()[t]) < 0 :
-#                     production = (stock - plant.minstock()*0.1)/float(data.timestepduration()[t])
-#                 p2_sol[(i, t)] = production
-#                 stock -= production * float(data.timestepduration()[t])
-#                 s_sol[(i, t)] = stock
-#                 productiontot[t] += production
-
-#         for i in range(data.nbpower1()):
-#             plant = data.accessPower1(0,i)
-#             for t in range(T):
-#                 reste = data.accessScenario(0).demands()[t]- productiontot[t]
-#                 sep = reste / data.nbpower1()
-#                 p1_sol[(i, t)] = sep
-
-#         return p1_sol, p2_sol, r_sol, s_sol
-
-#     def solve(self, data: Readingfile, scenario: int) -> Solution:
-#         start_time = time.time()
-#         result = self.scheduleMaintenance(data)
-
-#         if result is None:
-#             return None
-
-#         y_it, x_itk = result
-#         p1_sol, p2_sol, r_it, s_it = self.computeProductionPlan(data, y_it, x_itk)
-
-#         y_sol = {
-#             (i, t): 1
-#             for i in range(data.nbpower2())
-#             for t in range(data.timestep())
-#             if y_it[i][t] == 1
-#         }
-#         x_sol = {
-#             (i, k_index, t_start): 1
-#             for i, campaigns in enumerate(x_itk)
-#             for k_index, t_start in campaigns
-#         }
-
-#         sol = [p1_sol, p2_sol, y_sol, r_it, s_it, x_sol]
-
-#         runtime = time.time() - start_time
-
-#         # calcul de la fct obj
-#         I1 = range(data.nbpower1())   
-#         I2 = range(data.nbpower2())
-#         T = range(data.timestep())
-#         campaign_ids_by_unit = [range(len(data.accessPower2(i).Campaigns())) for i in I2]
-#         horizon_last_t = data.timestep() - 1
-#         K_i = []
-#         for i in I2:
-#             campaigns_i = []
-        
-#             for k in campaign_ids_by_unit[i]:
-#                 start = max(0, data.accessCampaign(i, k).earlieststop())
-#                 end   = min(horizon_last_t, data.accessCampaign(i, k).lateststop())
-            
-#                 k_range = list(range(start, end + 1))
-            
-#                 if k_range != [0]:   
-#                     campaigns_i.append(k_range)
-        
-#             K_i.append(campaigns_i)
-#         K_i_simple = {}
-#         for i in I2:
-#             K_i_simple[i] = [t for campagne in K_i[i] for t in campagne]
-#         RefCost_ik = [
-#                 [float(data.accessCampaign(i, k).refuelingcost()) 
-#                     for k in range(len(data.accessPower2(i).Campaigns()))
-#                 ]
-#             for i in I2
-#             ]
-#         Cost_it = [
-#         [data.accessPower1(scenario, i).cost()[t] for t in T]
-#         for i in I1]  # Cost_it[i][t]
-#         D_t = data.timestepduration()
-
-#         obj_value = (
-#             sum(
-#                 Cost_it[i][t] * p1_sol.get((i, t), 0.0) * D_t[t]
-#                 for i in I1
-#                 for t in T
-#             )
-#             +
-#             sum(
-#                 RefCost_ik[i][k_idx] *
-#                 sum(r_it.get((i, t), 0.0) for t in K_i[i][k_idx])
-#                 for i in I2
-#                 for k_idx in range(len(K_i[i]))
-#             )
-#         )
-
-#         return Solution("HEURISTIC_1", 
-#                     obj_value, 
-#                     float('inf'), runtime, sol)
-    
 # -----------------------------
-#  Heuristic 2 basic
+#  Heuristic basic
 # -----------------------------
-class MaintenanceHeuristicV2_basic(AbstractMaintenanceHeuristic):
+class MaintenanceHeuristic_basic(AbstractMaintenanceHeuristic):
     def computePriorityScores(self, data: Readingfile) -> list[tuple[int, float]]:
         w_i_tab = []
         tot_Pmax_ik = [
@@ -688,9 +411,9 @@ class MaintenanceHeuristicV2_basic(AbstractMaintenanceHeuristic):
         return Solution(f"HEURISTIC_2_{status}", obj_value, dual_bound, total_runtime + lp_runtime, sol)
 
 # -----------------------------
-#  Heuristic 2_2 
+#  Heuristic MultiCampaign 
 # -----------------------------
-class MaintenanceHeuristicV2MultiCampaign(MaintenanceHeuristicV2_basic):
+class MaintenanceHeuristic_MultiCampaign(MaintenanceHeuristic_basic):
     def findFeasibleStartTime(
         self,
         data: Readingfile,
@@ -813,9 +536,9 @@ class MaintenanceHeuristicV2MultiCampaign(MaintenanceHeuristicV2_basic):
         return Solution(f"HEURISTIC_2_MultiCampaign_{status}", obj_value, dual_bound, total_runtime + lp_runtime, sol)
     
 # -----------------------------
-#  Heuristic 2_3
+#  Heuristic OptXandY
 # -----------------------------
-class MaintenanceHeuristicV2MultiCampaign_2(MaintenanceHeuristicV2MultiCampaign):
+class MaintenanceHeuristic_OptXandY(MaintenanceHeuristic_MultiCampaign):
      def solve(self, data: Readingfile, scenario: int) -> Solution:
         self.set_data_attrs(data, scenario)
         start_time = time.time()
@@ -872,9 +595,9 @@ class MaintenanceHeuristicV2MultiCampaign_2(MaintenanceHeuristicV2MultiCampaign)
 
 
 # -----------------------------
-#  Heuristic 2_4
+#  Heuristic Random Forest models
 # -----------------------------
-class MaintenanceHeuristicV2_RF(MaintenanceHeuristicV2_basic):
+class MaintenanceHeuristic_RF(MaintenanceHeuristic_basic):
     """
     heuristic improved version using Random Forest models
     Pipeline:
@@ -1133,9 +856,9 @@ class MaintenanceHeuristicV2_RF(MaintenanceHeuristicV2_basic):
         )
 
 # -----------------------------
-#  Heuristic 3 (dichotomie)
+#  Heuristic relaxLagrange
 # -----------------------------
-class MaintenanceHeuristicV3_dichotomie(MaintenanceHeuristicV2_basic):
+class MaintenanceHeuristic_relaxLagrange(MaintenanceHeuristic_basic):
 # ======= INITIALISATION DES MODÈLES (UNE SEULE FOIS) =======
     def __init__(self, data, scenario):
         super().__init__()
